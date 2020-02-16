adb1ce06f21f12188a8948b57525aed3f4d22ce5
Andrew Bradjan <a2bradjan@gmail.com>
2020-02-15 19:16:03 -0800

    Fixed html validator so that it works

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 
-
 def validate_html(html):
     '''
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
@@ -10,6 +9,28 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
+    html = _extract_tags(html)
+    index = 0
+    while index < len(html)-1:
+        e = html[index]
+        f = "<" + "/" + e[1:]
+        g = html[index+1]
+
+        if g != f:
+            e = g
+            f = "<" + "/" + e[1:]
+            index+=1
+            if e[1:2] == "/":
+                return False
+        else:
+            html.remove(f)
+            html.remove(e)
+            index=0
+    if len(html) != 0:
+        return False
+    else:
+        return True
+
 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
@@ -17,15 +38,20 @@ def validate_html(html):
     # the main difference between your code and the book's code will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
 
-
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
-
-    This function returns a list of all the html tags contained in the input string,
-    stripping out all text not contained within angle brackets.
-
-    >>> _extract_tags('Python <strong>rocks</strong>!')
-    ['<strong>', '</strong>']
     '''
+    s = []
+    listhtml = list(html)
+    index = 0
+    while index < len(listhtml):
+        e = listhtml[index]
+        if e == '<':
+            string = ''
+            while listhtml[index] != '>':
+                string = string + listhtml[index]
+                index = index + 1
+            string = string + listhtml[index]
+            s.append(string)
+        index = index + 1
+    return s
